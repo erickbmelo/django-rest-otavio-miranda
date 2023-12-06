ab136ac17469a4eff25553217914bdc245ebb3c9
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-12-06 09:26:04 -0300

    Instalando o Django Rest Framework + INSTALLED_APPS

--- project/settings/installed_apps.py
+++ project/settings/installed_apps.py
@@ -1,12 +1,15 @@
 INSTALLED_APPS = [
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # A gente tem apps também
+    # Estes são nossos apps
     'recipes',
     'authors',
     'tag',
+    # Django REST Framework
+    'rest_framework',
 ]
